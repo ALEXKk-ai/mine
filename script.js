5784311968e4abbbab2969a773655e8fe92793fb
ALEXKk-ai <kimanialex002@gmail.com>
2024-10-10 14:54:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleCategories').addEventListener('click', function() {
+    var subCategories = document.getElementById('subCategories');
+    subCategories.classList.toggle('show'); // Toggle the 'show' class to display/hide subcategories
+});
